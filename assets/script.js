173742c02b9a163b53a1a8c1a290f8ff0d1c8ab8
Tuji Abdulaya <tujiabdulaya1@gmail.com>
2023-04-02 14:42:36 -0500

    quiz page setup

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var startBtn = document.getElementById('start')
+
+
+
+// var hi = "hi"
+
+function startQuiz() {
+    console.log ('the button got clicked');
+
+}
+
+startBtn.addEventListener('click', startQuiz)
